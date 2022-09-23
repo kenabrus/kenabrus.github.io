c5e93bcb905ffe77fa1c31aeaac859a27f05ab75
Andrzej Urbanek <kenabrus@gmail.com>
2022-09-23 15:38:17 +0200

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 const routes: Routes = [
-//   path: '', component: AppLayoutComponent,
-//   children: [
-//       { path: '', loadChildren: () => import('./demo/components/dashboard/dashboard.module').then(m => m.DashboardModule) },
-//   ],
-// },
-];
+  {path: '', component: LayoutComponent,
+  children: [
+      { path: '', loadChildren: () => import('./layout/layout.module').then(m => m.LayoutModule) },
+  ]}
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -3,6 +3,28 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { MenuComponent } from './menu/menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+
+import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MenubarModule } from 'primeng/menubar';
+import {TabMenuModule} from 'primeng/tabmenu';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar'
+import {RadioButtonModule} from 'primeng/radiobutton';
+
+
+import { StyleClassModule } from 'primeng/styleclass';
+import { DividerModule } from 'primeng/divider';
+import { ChartModule } from 'primeng/chart';
+import { PanelModule } from 'primeng/panel';
+import { ConfigComponent } from './config/config.component';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { FormsModule } from '@angular/forms';
+
 
 
 
@@ -10,10 +32,27 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [
     LayoutComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    ContentComponent,
+    ConfigComponent
   ],
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    TabMenuModule,
+    InputTextModule,
+    ButtonModule,
+    RouterModule,
+    DividerModule,
+    StyleClassModule,
+    ChartModule,
+    PanelModule,
+    SidebarModule,
+    InputSwitchModule,
+    RadioButtonModule,
+    FormsModule
   ],
   exports: [
     LayoutComponent
